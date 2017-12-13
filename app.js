43d5d182cff90419a2f23e1779a693b71510a687
EdwardGoomba <ask@edwardvdesign.com>
2017-12-13 10:14:16 -0500

    Capitalize list item on hover

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton.document.querySelector('button.removeItemBUtton');
+const removeItemButton = document.querySelector('button.removeItemBUtton');
+const listItems = document.getElementsByTagName('li')[0];
+
+listItems.addEventListener('mouseover', () => {
+	listItems.textContent = listItems.textContent.toUpperCase();
+});
+
+listItems.addEventListener('mouseout', () => {
+	listItems.textContent = listItems.textContent.toLowerCase();
+});
+
 
 toggleList.addEventListener('click', () => {
 	if (listDiv.style.display == 'none') {
@@ -28,10 +38,4 @@ addItemButton.addEventListener('click', () => {
 	li.textContent = addItemInput.value;
 	ul.appendChild(li);
 	addItemInput.value = '';
-});
-
-removeItemButton.addEventListener('click', () => {
-	let ul = document.getElementsByTagName('ul')[0];
-	let li = document.querySelector('li:last-child');
-	ul.removeChild(li);
 });
